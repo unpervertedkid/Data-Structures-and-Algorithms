8d95b1af4b88509fa50a733ae445a3014b5a2460
Brian Silah <unpervertedkid@gmail.com>
2022-10-20 11:26:52 +0300

    144. Binary Tree Preorder Traversal

---
+++ src/main/java/leetcode/trees/BinaryTreeInorderTraversal.java
@@ -0,0 +1,28 @@
+package leetcode.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> visitedNodes = new ArrayList<>();
+        if(root == null){
+            return visitedNodes;
+        }
+        else{
+            traverse(root,visitedNodes);
+        }
+        return visitedNodes;
+    }
+    private void traverse(TreeNode root, List<Integer> visitedNodes){
+        if(root == null){
+            return;
+        }
+        //Add root
+        visitedNodes.add(root.val);
+        //Go left
+        traverse(root.left,visitedNodes);
+        //Go right
+        traverse(root.right,visitedNodes);
+    }
+}
